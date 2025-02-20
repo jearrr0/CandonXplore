e012950ff33237af0eb701414d6a0f98866d5a1b
romeogacusan09@gmail.com <romeogacusan09@gmail.com>
2025-02-21 00:23:56 +0800

    initial commit

---
+++ Hotels/Hotels.js
@@ -0,0 +1,22 @@
+let slideIndex = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let slideId = ["mySlides0", "mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9"];
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) { slideIndex[no] = 1 }
+  if (n < 1) { slideIndex[no] = x.length }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
+}
+
+// Initialize slideshows
+for (let i = 0; i < slideIndex.length; i++) {
+  showSlides(1, i);
+}
